1c79768a89ec693a545e579b3fc7c39f7edb2251
kamilgurgener <kamilgurgener@gmail.com>
2023-02-12 13:15:39 -0500

    Added main method and a condition

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "Kamil";
+
+        if(str.equals("Kamil")){
+            System.out.println("Hello world!");
+        }else{
+            System.out.println("NOT Hello world!");
+        }
+
+
+    }
+}
