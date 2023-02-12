a2372d933ca6e939b6f6c075154299853734d110
kamilgurgener <kamilgurgener@gmail.com>
2023-02-12 13:59:47 -0500

    Updated Variable value

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        String str = "Hasan";
+        String str = "Mehmet";
 
         if(str.equals("Kamil")){
             System.out.println("Hello world!");
